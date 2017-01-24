f73ab3b3eeb4a85f96cb90f433e10bd5e00a4f8b
fishingelephants <eightminds@gmail.com>
2017-01-24 16:40:09 +0800

    Implemented enum for status into posts for approval workflow

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,6 @@
 # All Administrate controllers inherit from this `Admin::ApplicationController`,
 # making it the ideal place to put authentication logic or other
-# before_actions.
+# before_filters.
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
@@ -8,10 +8,10 @@ module Admin
   def self.admin_types
     ['AdminUser']
   end
-  
+
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!
-    before_filter :authenticate_admin
+    before_action :authenticate_admin
 
     def authenticate_admin
       unless Admin.admin_types.include?(current_user.try(:type))
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111133351) do
+ActiveRecord::Schema.define(version: 20170124082655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +18,10 @@ ActiveRecord::Schema.define(version: 20170111133351) do
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
+    t.integer  "status",     default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
