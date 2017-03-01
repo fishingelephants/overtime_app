7855589549906bd0ee110047ccce935dbf510c0c
fishingelephants <eightminds@gmail.com>
2017-03-01 13:47:58 +0800

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ end
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: User.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
